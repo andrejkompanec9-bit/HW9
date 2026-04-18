cb70ec3753d8ad42be198c1bccedfa3f7fb40576
andrejkompanec9-bit <andrejkompanec9@gmail.com>
2026-04-18 16:00:56 +0300

    чет нечет через методы

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        print ();
     }
+    private static void print () {
+        System.out.println(getInput() % 2 == 0);
+    }
+    public static int getInput() {
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextInt();
+    }
+
 }
